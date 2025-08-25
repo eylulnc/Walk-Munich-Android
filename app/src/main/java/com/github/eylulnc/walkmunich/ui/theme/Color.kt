ed3a3a22e225dcffec961357e65d3e3970616eca
Eylül Naz Can <eylulnc@gmail.com>
2025-08-25 16:28:12 +0200

    add app colors

--- app/src/main/java/com/github/eylulnc/walkmunich/ui/theme/Color.kt
+++ app/src/main/java/com/github/eylulnc/walkmunich/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.github.eylulnc.walkmunich.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BlueLight   = Color(0xFF8ECAE6)
+val BlueTeal    = Color(0xFF219EBC)
+val BlueNavy    = Color(0xFF023047)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Yellow      = Color(0xFFFFB703)
+val OrangeMain  = Color(0xFFFB8500) // ← primary
+val RedOrange   = Color(0xFFEC4E20)
+
+val Green       = Color(0xFF248232)
+val GreenLight  = Color(0xFF9ECE9A)
+
+// Neutrals
+val BgLight     = Color(0xFFFFFFFF)
+val BgDark      = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1B1B1B)
