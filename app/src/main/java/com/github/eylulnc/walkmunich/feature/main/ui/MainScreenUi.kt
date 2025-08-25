cb7718ba75f6e523c7f86a63444e156d089bdb79
Eylül Naz Can <eylulnc@gmail.com>
2025-08-25 21:51:55 +0200

    inject viewModel and and images

--- app/src/main/java/com/github/eylulnc/walkmunich/feature/main/ui/MainScreenUi.kt
+++ app/src/main/java/com/github/eylulnc/walkmunich/feature/main/ui/MainScreenUi.kt
@@ -1,9 +1,12 @@
 package com.github.eylulnc.walkmunich.feature.main.ui
 
 import androidx.compose.runtime.Composable
+import com.github.eylulnc.walkmunich.feature.main.viewModel.MainScreenViewModel
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MainScreenUi(
+    viewModel: MainScreenViewModel = koinViewModel(),
     onCategoryClick: (Long) -> Unit,
     onFavoritesItemClick: (Long) -> Unit) {
 }
