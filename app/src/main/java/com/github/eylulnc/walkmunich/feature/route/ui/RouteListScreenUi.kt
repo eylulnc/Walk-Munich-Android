44ab7b3b8dd819d8978bc08fc85519c323e785d0
Eylül Naz Can <eylulnc@gmail.com>
2025-08-26 21:36:09 +0200

    fix imports

--- app/src/main/java/com/github/eylulnc/walkmunich/feature/route/ui/RouteListScreenUi.kt
+++ app/src/main/java/com/github/eylulnc/walkmunich/feature/route/ui/RouteListScreenUi.kt
@@ -15,10 +15,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
