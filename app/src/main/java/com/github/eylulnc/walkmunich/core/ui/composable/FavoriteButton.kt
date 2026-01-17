4a5df760d3e30bcdb3211b3936c42695586de76a
Eylül Naz Can <eylulnc@gmail.com>
2026-01-17 02:10:15 +0100

    optimize imports

--- app/src/main/java/com/github/eylulnc/walkmunich/core/ui/composable/FavoriteButton.kt
+++ app/src/main/java/com/github/eylulnc/walkmunich/core/ui/composable/FavoriteButton.kt
@@ -1,12 +1,10 @@
 package com.github.eylulnc.walkmunich.core.ui.composable
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
@@ -14,7 +12,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import com.github.eylulnc.walkmunich.core.ui.theme.Spacing
 
